c99703fab4bac494d802d4ba647dbdcd8060303a
Antonio Souza <arfs.antonio@gmail.com>
2026-02-02 23:30:37 -0500

    Bump loshell to 0.2.1

--- Formula/loshell.rb
+++ Formula/loshell.rb
@@ -1,8 +1,8 @@
 class Loshell < Formula
   desc "Terminal-native ambient environment for deep focus with lofi radio and pomodoro"
   homepage "https://github.com/arferreira/loshell"
-  url "https://github.com/arferreira/loshell/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "ec836348b98c84eda0363da0e8550cf7e6b5c957a67f4bc18f26cc29325e2e10"
+  url "https://github.com/arferreira/loshell/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "175a8e72833d95ff18e9523e7112b047ce1289e996432f372013a8872d839b12"
   license "MIT"
 
   depends_on "rust" => :build
