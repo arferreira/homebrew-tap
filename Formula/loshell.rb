a3eab2ab80eb085415e844b3a35aac42eac4b1fc
Antonio Souza <arfs.antonio@gmail.com>
2026-02-10 20:59:45 -0500

    Update loshell to 0.3.1

--- Formula/loshell.rb
+++ Formula/loshell.rb
@@ -1,8 +1,8 @@
 class Loshell < Formula
   desc "Terminal-native ambient environment for deep focus with lofi radio and pomodoro"
   homepage "https://github.com/arferreira/loshell"
-  url "https://github.com/arferreira/loshell/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "deaf12b2f805c325c5592a11711fb62ba8742b8b35c7524e9f1c3bb69c05b341"
+  url "https://github.com/arferreira/loshell/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "2c7a50abb7a309e26405e16d2d6741a71b6e4eb2d9494fb35f67d020f23e587a"
   license "MIT"
 
   depends_on "rust" => :build
